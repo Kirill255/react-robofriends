16c3dcd89429f992e98ffb58f05832d1708b0252
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-21 12:37:27 +0300

    cardlist and card comp

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Card from "../Card/Card";
+
+const CardList = ({ robots }) => {
+  const cards = robots.map((robot) => <Card key={robot.id} robot={robot} />);
+
+  return <div>{cards}</div>;
+};
+
+export default CardList;
