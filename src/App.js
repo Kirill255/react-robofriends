20cbe391d440c7ee8cbca076907e1804f1fefc52
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-21 13:19:30 +0300

    searchbox comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 
 import CardList from "./components/CardList/CardList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import { robots } from "./robots";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App tc">
+        <h1>Robofriends</h1>
+        <SearchBox />
         <CardList robots={robots} />
       </div>
     );
