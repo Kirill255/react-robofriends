8eee8a3a218c80e5941ce6ffc406514f781c6f76
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-22 16:48:34 +0300

    install/setup redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { createLogger } from "redux-logger";
 import "tachyons";
@@ -12,8 +12,9 @@ import { searchRobots } from "./reducers";
 
 const logger = createLogger();
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // создали store и передали в него reducer
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, composeEnhancers(applyMiddleware(logger)));
 
 // с помощью компонента Provider обернули всё наше приложение и передали ему store как props, Provider это компонент-обёртка, который позволяет не передавать store в каждый компонент по отдельности, а передать store только в Provider, после чего store будет доступен в каждом компоненте в дереве
 ReactDOM.render(
